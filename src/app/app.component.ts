23361f4ae5425d7beed46529a83a4f1242abf954
caioom <caio.ds.2003@gmail.com>
2022-11-27 14:00:00 -0300

    event binding, getting a button click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-generator';
+  onButtonClick (): void {
+    console.log('button was clicked')
+  }
 }

11c1190cddc340d305589944f62ddc1666e7fe44
caioom <caio.ds.2003@gmail.com>
2022-12-01 22:41:25 -0300

    managing checkbox state changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  includeLetters: boolean = false
+  includeNumbers: boolean = false
+  includeSymbols: boolean = false
   password: string = ''
 
   onButtonClick (): void {
     this.password = 'MY PASSWORD'
+  }
+
+  onChangeUseLetters (): void {
+    this.includeLetters = !this.includeLetters
+  }
+
+  onChangeUseNumbers (): void {
+    this.includeNumbers = !this.includeNumbers
+  }
+
+  onChangeUseSymbols (): void {
+    this.includeSymbols = !this.includeSymbols
   }
 }
